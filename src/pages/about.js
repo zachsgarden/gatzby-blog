7267e7db41812f5816b9bcc312b69b2e6a74e6c1
Zach Kamp <kamp.zach@gmail.com>
2022-05-26 11:48:13 -0400

    creating develop branch

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,6 @@
-// Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
 
-// Step 2: Define your component
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
@@ -11,5 +9,4 @@ const AboutPage = () => {
     )
 }
 
-// Step 3: Export your component
 export default AboutPage
